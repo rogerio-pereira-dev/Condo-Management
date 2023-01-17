c43652008670ac5cd19102670173282d2b5f6242
Rogerio Pereira <hi@rogeriopereira.dev>
2023-01-17 00:40:13 -0500

    Adding role to UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,6 +18,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'role' => $this->role,
         ];
     }
 }
