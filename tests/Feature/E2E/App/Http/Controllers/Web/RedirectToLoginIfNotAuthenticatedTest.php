12a486c3cc6751f6e05e188c43e605b14edfc52f
Rogerio Pereira <hi@rogeriopereira.dev>
2023-02-08 17:55:47 -0500

    Authenticated web routes Test

---
+++ tests/Feature/E2E/App/Http/Controllers/Web/RedirectToLoginIfNotAuthenticatedTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\E2E\App\Http\Controllers\Web;
+
+use Tests\TestCase;
+
+class RedirectToLoginIfNotAuthenticatedTest extends TestCase 
+{
+    public function urlDataProvider() : array
+    {
+        return [
+            'home'              => ['url' => '/'],
+            'change_password'   => ['url' => '/change-password'],
+            'employees'         => ['url' => '/employees'],
+        ];
+    }
+
+    /**
+     * @dataProvider urlDataProvider
+     */
+    public function testRedirectIfNotAuthenticated($url)
+    {
+        $this->assertGuest();
+        $this->get($url)
+            ->assertRedirect('/login');
+    }
+}
