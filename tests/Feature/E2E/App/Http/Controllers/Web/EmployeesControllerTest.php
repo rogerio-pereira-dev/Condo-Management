bfe11c2921c52678854da25a1154f487afedfa47
Rogerio Pereira <hi@rogeriopereira.dev>
2023-02-08 20:31:29 -0500

    Web  - EmployeeControllerTest

---
+++ tests/Feature/E2E/App/Http/Controllers/Web/EmployeesControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature\E2E\App\Http\Controllers\Web;
+
+use Tests\TestCase;
+use Inertia\Testing\AssertableInertia;
+
+class EmployeesControllerTest extends TestCase
+{
+    public function testRenderEmployeesPage()
+    {
+        $this
+            ->actingAs($this->userAdmin)
+            ->get('/employees')
+            ->assertInertia(fn (AssertableInertia $page) => 
+                $page->component('Employees/Index')
+            );
+    }
+}
